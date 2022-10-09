f983da218656022a8c3b160ff336be19bb5317b3
Riyash Patel <110080129+Riyash1405@users.noreply.github.com>
2022-10-09 15:05:25 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -9,12 +9,22 @@
 	// Create a connection
 	$conn = mysqli_connect($servername,$username, $password, $database);
 
-
-	if($conn) {
-		echo "success";
-	}
-	else {
-		die("Error". mysqli_connect_error());
-	}
-
+    if($_POST['register'])
+    {
+     $username= $_POST['username'];
+     $pwd= $_POST['password'];
+     $cpwd=$_POST['cpassword'];
+   
+    $query= "INSERT INTO users VALUES ('$username','$pwd','$cpwd')";
+   $data=mysqli_query($conn,$query);
+   
+   if($data)
+   {
+     echo "data inserted into database";
+   }
+   else{
+     echo "failed";
+   }
+   
+   }
    
