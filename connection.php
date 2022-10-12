623d958c3ae76e329406f0a460a17ed36997991e
Riyash Patel <110080129+Riyash1405@users.noreply.github.com>
2022-10-12 18:00:07 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php      
+    
+      
+    $con = mysqli_connect("localhost", "root", '', "Login");  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
+?>  
